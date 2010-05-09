40f4aacb8a1d61a479a11f4f89602f4826829dce
rick <technoweenie@gmail.com>
2010-05-09 11:01:58 -0700

    initial Client initializer

---
+++ test/client_test.js
@@ -0,0 +1,28 @@
+var assert = require('assert')
+  , Client = require('../lib/client').Client
+
+var cli = new Client()
+assert.equal('127.0.0.1', cli.host)
+assert.equal(8098,        cli.port)
+assert.equal('/riak/',    cli.prefix)
+assert.equal('/mapred',   cli.mapred)
+assert.ok(cli.clientID)
+
+cli = new Client('http://server.com:4567/boom')
+assert.equal('server.com', cli.host)
+assert.equal(4567,         cli.port)
+assert.equal('/boom/',     cli.prefix)
+
+cli = new Client('http://server.com:4567/boom', {host: 'abc', mapred: 'map', clientID: 'def'})
+assert.equal('server.com', cli.host)
+assert.equal(4567,         cli.port)
+assert.equal('/boom/',     cli.prefix)
+assert.equal('/map',       cli.mapred)
+assert.equal('def',        cli.clientID)
+
+cli = new Client({host: 'server.com', port: 4567, prefix: 'boom', mapred: 'map', clientID: 'def'})
+assert.equal('server.com', cli.host)
+assert.equal(4567,         cli.port)
+assert.equal('/boom/',     cli.prefix)
+assert.equal('/map',       cli.mapred)
+assert.equal('def',        cli.clientID)
