e33be2898d3bbef4909d2518df357962285a9f71
rick <technoweenie@gmail.com>
2010-05-12 00:32:51 -0700

    add link header encoding

--- test/link_parsing_test.js
+++ test/link_parsing_test.js
@@ -1,4 +1,5 @@
 var assert = require('assert')
+  ,   nori = require('../lib')
   ,   Link = require('../lib/resource').Link
 
 header = '</riak/armor>; rel="up", </riak/foo/bar>; riaktag="blah"'
@@ -15,3 +16,12 @@ assert.equal('foo',   links[1].bucket)
 assert.equal('bar',   links[1].key)
 assert.equal(null,    links[1].rel)
 assert.equal('blah',  links[1].riaktag)
+
+var client = nori.client()
+  ,    res = {"client":client}
+  ,   link = (new Link(res, {bucket: 'foo', key: 'bar', tag: 'baz'}))
+
+assert.equal('</riak/foo/bar>; riaktag="baz"', link.encode())
+
+link = new Link(res, {bucket: 'foo', rel: 'baz'})
+assert.equal('</riak/foo>; rel="baz"', link.encode())
